097c306ab62100c139b4b7e243a236e927aaed35
Braden <bradenleach111@gmail.com>
2025-04-23 08:32:33 -0400

    Update class.cs

--- NMC_C#/class.cs
+++ NMC_C#/class.cs
@@ -1,11 +1,13 @@
 public class Book
 {
+    // Changable Variables (Info to Describe Book) // 
     public string Title { get; set; }
     public string Author { get; set; }
     public string Genre { get; set; }
     public int PageCount { get; set; }
     public double Rating { get; set; }
 
+    // Default Values // 
     public Book()
     {
         Title = "Unknown";
@@ -15,6 +17,7 @@ public class Book
         Rating = 0.0;
     }
 
+    // Input Values for Book // 
     public Book(string title, string author, string genre, int pageCount, double rating)
     {
         Title = title;
@@ -24,6 +27,7 @@ public class Book
         Rating = rating;
     }
 
+    // Book Summary // 
     public string GetBookSummary()
     {
         return $"{Title} by {Author} ({Genre}, {PageCount} pages, Rating: {Rating}/5)";
